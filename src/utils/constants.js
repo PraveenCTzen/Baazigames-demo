1f14dd998b400adaae09893aa4c7b7de035b1f2d
praveen.dangwani <praveen.dangwani@clevertap.com>
2023-08-09 16:29:12 +0530

    jobyoda creds

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,13 @@
 export const ctAccountAndRegions = [
   // { label: "in1", value: "in1" },
   // { label: "sk1", value: "sk1"},
-  // { label: "sg1", value: "sg1" },
-  { label: "eu1", value: "eu1" },
+  { label: "sg1", value: "sg1" },
+  // { label: "eu1", value: "eu1" },
   // { label: "us1", value: "us1"}
 ]
 
-export const scAccountId = "649935be77a9649a5129f8d5"
-export const scApikey = "P7bUJmqC5KxsDrW3ukufcs9ZMShknHxZzOO34x1F30Kk60vdZJ60MEFnheqOHFuT"
+// export const scAccountId = "649935be77a9649a5129f8d5"
+// export const scApikey = "P7bUJmqC5KxsDrW3ukufcs9ZMShknHxZzOO34x1F30Kk60vdZJ60MEFnheqOHFuT"
+
+export const scAccountId = "63fc6403e929d558de79c5af"
+export const scApikey = "gLYyeN0pGk5Ifp6DD4TuG28YRPhzNAKkGJRYN3otHH7LxC5BIyKFLlz0NAEBw7Eq"
